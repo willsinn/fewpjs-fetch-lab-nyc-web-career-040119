3af0b032b3a1b1ae1edaca2bcf375bf8e298bd82
William Sinn <w.h.sinn@gmail.com>
2019-05-16 17:35:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function fetchBooks() {
 
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
